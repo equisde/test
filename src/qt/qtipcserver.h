b188c77c23eab24161a4070c9e192623762f6ad0
root <root@astra1602.dedicatedpanel.com>
2017-11-15 12:26:55 +0100

    reup

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Dogit-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "DogitURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
