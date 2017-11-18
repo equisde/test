b87428950262347972edd1a9ddfdae54e8b72b80
kensukekita <rbert379@gmail.com>
2017-11-18 11:09:25 -0300

    rolling back

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 39871 : 29871;
+    return testnet ? 33185 : 23185;
 }
 
 
