ce6b91a2caae5e6ec428aaebf747a31a4a8f3861
kensukekita <rbert379@gmail.com>
2017-11-17 22:44:34 -0300

    port updated
    port updated

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 33185 : 23185;
+    return testnet ? 39871 : 29871;
 }
 
 
