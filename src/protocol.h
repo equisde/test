e0ea5e34611f3f00d07f80b3b93b74d55c3b0803
kensukekita <rbert379@gmail.com>
2017-12-03 20:50:33 -0300

    test
    test

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 33185 : 23185;
+    return testnet ? 33445 : 22445;
 }
 
 
