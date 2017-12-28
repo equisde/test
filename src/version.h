1fdd7f2827e545c05f15eb5324214d7c93bb0eb6
equisde <34652149+equisde@users.noreply.github.com>
2017-12-28 17:12:35 -0300

    Update version.h

--- src/version.h
+++ src/version.h
@@ -30,14 +30,14 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60017;
+static const int PROTOCOL_VERSION = 60018;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PROTO_VERSION = 60015;  // v1.3 and 1.4
-static const int MIN_PROTO_VERSION_FORK = 60016; // v1.4.1 up
+static const int MIN_PROTO_VERSION = 60017;  // v1.3 and 1.4
+static const int MIN_PROTO_VERSION_FORK = 60017; // v1.4.1 up
 static const int MIN_PROTO_VERSION_FORK_2 = 60017; // v1.5 up
 
 // nTime field added to CAddress, starting with this version;
@@ -45,14 +45,14 @@ static const int MIN_PROTO_VERSION_FORK_2 = 60017; // v1.5 up
 static const int CADDR_TIME_VERSION = 31402;
 
 // only request blocks from nodes outside this range of versions
-static const int NOBLKS_VERSION_START = 60002;
-static const int NOBLKS_VERSION_END = 60013;
-static const int NOBLKS_VERSION_END_FORK_2 = 60016; // share nodes but no blocks
+static const int NOBLKS_VERSION_START = 60017;
+static const int NOBLKS_VERSION_END = 60017;
+static const int NOBLKS_VERSION_END_FORK_2 = 60017; // share nodes but no blocks
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 60000;
+static const int BIP0031_VERSION = 60017;
 
 // "mempool" command, enhanced "getdata" behavior starts with this version:
-static const int MEMPOOL_GD_VERSION = 60002;
+static const int MEMPOOL_GD_VERSION = 60017;
 
 #endif
